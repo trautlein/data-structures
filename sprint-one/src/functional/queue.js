ec4270e7d32f7d031b79988802b89a1f8b497499
trautlein <hans@trautlein.com>
2016-09-14 15:43:52 -0700

    solved all tests for queue in functional except one
    Stuck on the final test about queue-specific behavior, where you
    remove the oldest item after newer items have already been added
    and then removed

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,7 @@
 var Queue = function() {
   var someInstance = {};
+  var counter = 0;
+  var removed = 1; // haven't actually removed one yet!
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +9,22 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    counter++;
+    storage[counter] = value;
   };
 
   someInstance.dequeue = function() {
+    if (counter > 0) {
+      var dequeued = storage[removed];
+      delete storage[removed];
+
+      counter--; 
+      return dequeued;
+    }
   };
 
   someInstance.size = function() {
+    return counter;
   };
 
   return someInstance;
