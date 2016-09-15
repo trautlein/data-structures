ef787e838abda48d7f3c45f2c9bbed740bafc774
trautlein <hans@trautlein.com>
2016-09-14 21:10:21 -0700

    Finished functional-shared queue working on stack

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,37 @@
-var Queue = function() {
+var Queue = function(enqueue, dequeue, size) {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // but try not to reference your old code in writing the new style.
+
+  var newQueue = {};
+
+  newQueue.counter = 0;
+  newQueue.remove = 1;
+  newQueue.enqueue = enqueue;
+  newQueue.dequeue = dequeue;
+  newQueue.size = size;
+
+  _.extend(newQueue, queueMethods);
+
+  return newQueue;
 };
 
 var queueMethods = {};
 
 
+queueMethods.size = function () {
+  return this.counter;
+};
+
+queueMethods.enqueue = function (value) {
+  this.counter++;
+  this[this.counter] = value;
+};
+
+queueMethods.dequeue = function () {
+  if (this.counter > 0) {
+    var popped = this[this.counter];
+    delete this[this.counter];
+    this.counter--;
+    return popped;
+  }
+};
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,37 @@
-var Stack = function() {
+var Stack = function(push, pop, size) {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // but try not to reference your old code in writing the new style.
+
+  var newStack = {};
+
+  newStack.counter = 0;
+  newStack.push = push;
+  newStack.pop = pop;
+  newStack.size = size;
+
+  _.extend(newStack, stackMethods);
+
+  return newStack;
 };
 
 var stackMethods = {};
 
+
+stackMethods.size = function () {
+  return this.counter;
+};
+
+stackMethods.push = function (value) {
+  this.counter++;
+  this[this.counter] = value;
+};
+
+stackMethods.pop = function () {
+  if (this.counter > 0) {
+    var popped = this[this.counter];
+    delete this[this.counter];
+    this.counter--;
+    return popped;
+  }
+};
 
