c1573465a6aa7f8a8f10bdbca898cfa4f808abaf
trautlein <hans@trautlein.com>
2016-09-15 10:19:41 -0700

    Finished the pseudoclassical queue and day 1 BMR

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,28 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.counter = 0;
+  this.remove = 1;
 };
 
+Queue.prototype.size = function() {
+  if ((this.counter - this.remove + 1) > 0) {
+    return this.counter - this.remove + 1;
+  }
+  return 0;
+};
 
+Queue.prototype.enqueue = function(value) {
+
+  this.counter++;
+  this[this.counter] = value;
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.counter > 0) {
+    var removed = this[this.remove];
+    delete this[this.remove];
+    this.remove++;
+    return removed;
+  }
+};
