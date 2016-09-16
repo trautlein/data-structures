4835db8a7b901ad3c45c0feaaf9d9e8888d170f2
trautlein <hans@trautlein.com>
2016-09-16 10:56:30 -0700

    Added a more deeply nested test for tree.contains

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -41,8 +41,11 @@ describe('tree', function() {
     tree.addChild(6);
     tree.children[0].addChild(7);
     tree.children[1].addChild(8);
+    tree.children[0].children[0].addChild(9);
     expect(tree.contains(7)).to.equal(true);
     expect(tree.contains(8)).to.equal(true);
+    expect(tree.contains(9)).to.equal(true);
+
   });
 
 });
