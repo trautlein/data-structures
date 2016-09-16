ca8201ddbce8b20435ff7e247144ba0502d8fdcb
trautlein <hans@trautlein.com>
2016-09-16 12:30:20 -0700

    Implemented the first functions for a graph
    addNode, removeNode, hasEdge, addEdge were all created here

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,39 +1,51 @@
-
-
 // Instantiate a new graph
 var Graph = function() {
+  this.nodes = {};
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this[node] = [];
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  if (this.hasOwnProperty(node)) {
+    return true;
+  }
+  return false;
 };
-
+   
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  delete this[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  for (var i = 0; i < this[fromNode].length; i++) {
+    if (this[fromNode][i] === toNode) {
+      return true;
+    }
+  }
+  return false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this[fromNode].push(toNode);
+  this[toNode].push(fromNode);
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
 };
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
+// Complexity: What is the time complexity of the above functions?
 
 
