de6be9342f1a5a6955e91757ef06ebfc60aa06ad
Hans Trautlein <hans@trautlein.com>
2016-09-17 12:34:09 -0700

    Changed undefined to null

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,8 +1,8 @@
 var BinarySearchTree = function(root) {
   var newBST = Object.create(bstMethods);
   newBST.value = root;
-  newBST.left = undefined;
-  newBST.right = undefined;
+  newBST.left = null;
+  newBST.right = null;
   return newBST;  
 };
 
@@ -10,13 +10,13 @@ var bstMethods = {};
 
 bstMethods.insert = function(value) {
   if (this.value > value) {
-    if (this.left === undefined) {
+    if (this.left === null) {
       this.left = BinarySearchTree(value);
     } else {
       this.left.insert(value);
     }
   } else {
-    if (this.right === undefined) {
+    if (this.right === null) {
       this.right = BinarySearchTree(value);
     } else {
       this.right.insert(value);
@@ -28,9 +28,9 @@ bstMethods.contains = function(target) {
   if (this.value === target) {
     return true;
   } else {
-    if (this.value > target && this.left !== undefined) {
+    if (this.value > target && this.left !== null) {
       return this.left.contains(target);
-    } else if (this.value < target && this.right !== undefined) {
+    } else if (this.value < target && this.right !== null) {
       return this.right.contains(target);
     }
   }
@@ -39,10 +39,10 @@ bstMethods.contains = function(target) {
 
 bstMethods.depthFirstLog = function(func) {
   func(this.value);
-  if (this.left !== undefined) {
+  if (this.left !== null) {
     this.left.depthFirstLog(func);
   }
-  if (this.right !== undefined) {
+  if (this.right !== null) {
     this.right.depthFirstLog(func);
   }
 };
