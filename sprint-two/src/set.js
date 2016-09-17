6573b66809a5c5e1449795db6708aa42f11a5c21
trautlein <hans@trautlein.com>
2016-09-16 19:02:00 -0700

    Finished all methods in prototypal style for set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,32 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (this._storage.hasOwnProperty(item)) {
+    this._storage[item] += 1;
+  } else {
+    this._storage[item] = 1;
+  }
 };
 
 setPrototype.contains = function(item) {
+  if (this._storage.hasOwnProperty(item)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 setPrototype.remove = function(item) {
+  if (this._storage.hasOwnProperty(item)) {
+    delete this._storage[item];
+  }
 };
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
+
+// Complexity: What is the time complexity of the above functions?
