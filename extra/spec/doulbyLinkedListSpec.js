ddca6418ca03ac5873c37af4a81eb35454ce247a
trautlein <hans@trautlein.com>
2016-09-17 16:49:23 -0700

    Finished all requirements for Doubly Linked List

--- extra/spec/doulbyLinkedListSpec.js
+++ extra/spec/doulbyLinkedListSpec.js
@@ -1,12 +1,88 @@
-describe('doubly linked list', function() {
+describe('Doubly Linked List', function() {
   var dLL;
 
-  beforeEach(function() {
-    dLL = new DoublyLinkedList();
+  describe('doublylinkedList tests', function() {
+    var dLL;
+
+    beforeEach(function() {
+      dLL = new DoublyLinkedList();
+    });
+
+    it('new head should refer to previous head', function() {
+      dLL.addToHead(5);
+      dLL.addToHead(10);
+      dLL.addToHead(20);
+      dLL.addToHead(30);
+      expect(dLL.head.next.value).to.equal(20);
+    });
+
+    it('new tail should refer to previous tail', function() {
+      dLL.addToTail(5);
+      dLL.addToTail(12);
+      dLL.addToTail(200);
+      expect(dLL.tail.prev.value).to.equal(12);
+    });
+
+
+
   });
 
-  // it('should have methods named ..."', function() {
-  //   expect(graph.forEachNode).to.be.a('function');
-  // });
+
+  describe('linkedList tests', function() {
+    var dLL;
+
+    beforeEach(function() {
+      dLL = DoublyLinkedList();
+    });
+
+    it('should have a head and tail', function() {
+      expect(dLL).to.have.property('head');
+      expect(dLL).to.have.property('tail');
+    });
+
+    it('should have methods named "addToTail", "removeHead", and "contains"', function() {
+      expect(dLL.addToTail).to.be.a('function');
+      expect(dLL.removeHead).to.be.a('function');
+      expect(dLL.contains).to.be.a('function');
+    });
+
+    it('should designate a new tail when new nodes are added', function() {
+      dLL.addToTail(4);
+      expect(dLL.tail.value).to.equal(4);
+      dLL.addToTail(5);
+      expect(dLL.tail.value).to.equal(5);
+    });
+
+    it('should remove the head from the list when removeHead is called', function() {
+      dLL.addToTail(4);
+      dLL.addToTail(5);
+      expect(dLL.head.value).to.equal(4);
+      dLL.removeHead();
+      expect(dLL.head.value).to.equal(5);
+    });
+
+    it('should return the value of the former head when removeHead is called', function() {
+      dLL.addToTail(4);
+      expect(dLL.removeHead()).to.equal(4);
+    });
+
+    it('should contain a value that was added', function() {
+      dLL.addToTail(4);
+      dLL.addToTail(5);
+      expect(dLL.contains(4)).to.equal(true);
+      expect(dLL.contains(5)).to.equal(true);
+      expect(dLL.contains(6)).to.equal(false);
+    });
+
+    it('should not contain a value that was removed', function() {
+      dLL.addToTail(4);
+      dLL.addToTail(5);
+      dLL.removeHead();
+      expect(dLL.contains(4)).to.equal(false);
+    });
+
+    // add more tests here to test the functionality of linkedList
+  });
+
 
 });
--- extra/src/doulbyLinkedList.js
+++ extra/src/doulbyLinkedList.js
@@ -1,2 +1,76 @@
+var Node = function(value) {
+  var node = {};
+
+  node.value = value;
+  node.next = null;
+  node.prev = null; // how to best change this when added?
+
+  return node;
+};
+
 var DoublyLinkedList = function() {
+  var list = {};
+  list.head = null;
+  list.tail = null;
+
+  list.addToHead = function(value) {
+    var node = new Node(value);
+    if (!list.head) {
+      list.head = node;
+      list.tail = node;
+    } else {
+      node.next = list.head;
+      list.head.prev = node;
+      list.head = node;
+    }
+  };
+
+  list.addToTail = function(value) {
+    var node = new Node(value);
+    if (!list.head) {
+      list.head = node;
+      list.tail = node;
+    } else {
+      list.tail.next = node;
+      node.prev = list.tail;
+      list.tail = node;
+    }
+  };
+
+  list.removeHead = function() {
+    if (list.head.next === null) {
+      var oldHead = list.head;
+      list.head = null;
+      list.tail = null;
+      return oldHead.value;
+    } else {
+      var oldHead = list.head;
+      list.head = list.head.next;
+      list.head.prev = null;
+      return oldHead.value;
+    }
+  };
+
+  list.removeTail = function() {
+    var oldTail = list.tail;
+    list.tail = list.tail.prev;
+    list.tail.next = null;
+    return oldTail.value;
+  };
+
+  list.contains = function(target) {
+    var whereWeAreAt = list.head;
+
+    while (whereWeAreAt) {
+      if (whereWeAreAt.value === target) {
+        return true; 
+      } else {
+        whereWeAreAt = whereWeAreAt.next;
+      }
+    }
+    return false;
+  };
+
+
+  return list;
 };
