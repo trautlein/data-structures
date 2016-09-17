7fd01928b3011a1c9d552127ca9632be24854e13
Hans Trautlein <hans@trautlein.com>
2016-09-17 15:48:06 -0700

    Continued work on the skeleton, should be done

---
+++ extra/spec/doulbyLinkedListSpec.js
@@ -0,0 +1,12 @@
+describe('doubly linked list', function() {
+  var dLL;
+
+  beforeEach(function() {
+    dLL = new DoublyLinkedList();
+  });
+
+  // it('should have methods named ..."', function() {
+  //   expect(graph.forEachNode).to.be.a('function');
+  // });
+
+});
---
+++ extra/src/doulbyLinkedList.js
@@ -0,0 +1,2 @@
+var DoublyLinkedList = function() {
+};
