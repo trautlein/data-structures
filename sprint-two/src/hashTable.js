61726ba4eb7682010c1b51ac0d354b9c0a6ae63f
trautlein <hans@trautlein.com>
2016-09-16 20:54:58 -0700

    Started working on hashTable.js - a little stuck

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,16 +1,28 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  for (var z = 0; z < this._limit; z++) {
+    this._storage.set(z, []);
+  }
+
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var tuples = this._storage.get(index);
+
+  this._storage.set(index, tuples.concat([k, v]));
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  
+  var tuples = this._storage.get(index);
+  for (var i = 0; i < tuples.length; i++) {
+    if (tuples[i][0] === k) {
+      return tuples[i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
@@ -19,8 +31,5 @@ HashTable.prototype.remove = function(k) {
 
 
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
 
-
+// Complexity: What is the time complexity of the above functions?
